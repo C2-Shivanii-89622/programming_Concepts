824f7078a6c6796d4e60053f6f03615551ca768f
ShivaniPatil943 <patilshivani034@gmail.com>
2025-03-07 13:32:23 +0530

    Update Question6.py

--- Assingment5/Question6.py
+++ Assingment5/Question6.py
@@ -9,7 +9,7 @@ def count_frequencies(numbers):
     
     return count_dict
 
-numbers_list = [1, 2, 2, 2, 3, 4, 4, 4, 5, 5, 6, 6, 6, 7, 7, 8, 9, 23]
+numbers_list = [1, 2, 4, 2, 6, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 9, 23]
 
 output = count_frequencies(numbers_list)
 print(output)
